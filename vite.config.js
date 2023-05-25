1c6d4f736df2452fdd308d070469287c0d7ba2b3
kyzym <kyzym1@gmail.com>
2023-05-25 15:46:34 +0200

    fix: deploy 4  05/25/2023 15:46:34

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/kyzym/DeliveryAppFront/',
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    publicPath: '/',
+  },
 });
